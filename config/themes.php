8f7b5caed8b2b0b15af433252567bf8652e5a2e3
JeroenG <jeroengjeroeng@gmail.com>
2016-06-26 22:05:06 +0200

    CS fixes

--- config/themes.php
+++ config/themes.php
@@ -2,24 +2,24 @@
 
 return [
 
-    /**
+    /*
      * This is the default theme.
      * It is the last resort to look for files that aren't found elsewhere.
      */
     'default' => 'master',
 
-    /**
+    /*
      * The theme to be used can be set during runtime, but also here.
      */
     //'theme' => '',
 
-    /**
+    /*
      * Should the current theme path be cached?
      * If so, it will be cached until the Laravel cache is flushed.
      */
     'cache' => true,
 
-    /**
+    /*
      * These are the locations where you place your theme files and assets.
      */
     'locations' => [
